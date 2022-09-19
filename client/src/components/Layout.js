c6e5f2c264843c246634250d22fe431d68479d40
Kat Gomozova <kathy.gomozova@gmail.com>
2022-09-18 19:52:27 -0700

    admin and user menus

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../layout.css";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-//import {Navigate} from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { Badge } from "antd";
+import {Navigate} from 'react-router-dom';
 
 
 function Layout({children}) 
 {
+    const [collapsed, setCollapsed] = useState(false);
+    const { user } = useSelector((state) => state.user);
+    
+    const navigate = useNavigate();
     const location = useLocation();
     const userMenu = [
         {
@@ -27,44 +33,89 @@ function Layout({children})
             name: "Profile",
             path: "/profile",
             icon: "ri-user-line"
-        },
-        {
-            name: "Logout",
-            path: "/lpgout",
-            icon: "ri-login-box-line"
         }
     ];
 
-    const menuToBeRendered = userMenu;
+    const adminMenu = [
+        {
+          name: "Home",
+          path: "/",
+          icon: "ri-home-line",
+        },
+        {
+          name: "Users",
+          path: "/admin/userslist",
+          icon: "ri-user-line",
+        },
+        {
+          name: "Doctors",
+          path: "/admin/doctorslist",
+          icon: "ri-user-star-line",
+        },
+        {
+          name: "Profile",
+          path: "/profile",
+          icon: "ri-user-line",
+        }
+      ];
+
+      const menuToBeRendered = user?.isAdmin ? adminMenu : user?.isDoctor ? userMenu : userMenu;
+      const role = user?.isAdmin ? "Admin" : user?.isDoctor ? "Doctor" : "User";
 
     return (
-        <div className = "main"> 
-            <div className = "d-flex layout"> 
-                <div className = "sidebar"> 
-                    <div className = "sidebar-header">
-                        <h1> HC </h1> 
+        <div className="main">
+            <div className="d-flex layout">
+                <div className="sidebar">
+                    <div className="sidebar-header">
+                        <h1 className = "logo"> HC </h1>
+                        <h1 className="role">{role}</h1>
                     </div>
-                    <div className = "menu">
-                    {menuToBeRendered.map((menu) => 
-                    {
-                        const isActive = location.pathname === menu.path;
-                        return (
-                        <div className = {`d-flex menu-item ${
-                            isActive && "active-menu-item"
-                          }`}>
-                        <i className={menu.icon}></i>
-                        <Link to={menu.path}>{menu.name}</Link>
-                        </div>
+                    <div className="menu">
+                        {menuToBeRendered.map((menu) => {
+                            const isActive = location.pathname === menu.path;
+                            return (
+                                <div className={`d-flex menu-item ${
+                                    isActive && "active-menu-item"
+                                    }`}>
+                                    <i className={menu.icon}></i>
+                                    <Link to={menu.path}>{menu.name}</Link>
+                                </div>
                             );
-                    })}
+                        })}
+                        <div className={`d-flex menu-item `} onClick={() => {
+                            localStorage.clear();
+                            navigate("/login");
+                        }}>
+                            <i className="ri-logout-circle-line"></i>
+                            {!collapsed && <Link to="/login">Logout</Link>}
+                        </div>
                     </div>
                 </div>
-                <div className = "content">
-                    <div className = "header"> header</div>
-                    <div className = "body"> 
+                <div className="content">
+                    <div className="header">
+                        {collapsed ? (
+                            <i
+                                className="ri-menu-2-fill header-action-icon"
+                                onClick={() => setCollapsed(false)}
+                            ></i>
+                        ) : (
+                                <i
+                                    className="ri-close-fill header-action-icon"
+                                    onClick={() => setCollapsed(true)}
+                                ></i>
+                            )}
+
+                        <div className="d-flex align-items-center px-4">
+
+                            <Link className="anchor mx-2" to="/profile">
+                                {user?.name}
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="body">
                         {children}
                     </div>
-                 </div>
+                </div>
             </div>
         </div>
     )
