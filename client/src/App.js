83406a1f7059b7cbcb2be818bd7b57f6fd2debf3
Kat Gomozova <kathy.gomozova@gmail.com>
2022-10-09 21:48:00 -0700

    apply doctor

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Register from './pages/Register';
 import Home from './pages/Home';
 import ProtectedRoute from './components/ProtectedRoute';
 import PublicRoute from './components/PublicRoute';
+import ApplyDoctor from './pages/ApplyDoctor';
 
 function App() {
   const { loading } = useSelector((state) => state.alerts);
@@ -24,6 +25,7 @@ function App() {
           <Route path = '/login' element={<PublicRoute> < Login/> </PublicRoute>}/>
           <Route path = '/register' element={<PublicRoute> <Register/> </PublicRoute>}/>
           <Route path = '/' element={<ProtectedRoute> <Home/> </ProtectedRoute>}/>
+          <Route path = '/apply-doctor' element={<ProtectedRoute> <ApplyDoctor/> </ProtectedRoute>}/>
            </Routes>
       </BrowserRouter>
 
